366f64500599af88292c0b67912212565b941b20
Rajaraman Iyer <rajaramaniyer@gmail.com>
2025-09-26 08:52:35 +0800

    update generate-jpg

--- generate-jpg.py
+++ generate-jpg.py
@@ -10,14 +10,15 @@ import shutil
 FONT_PATH_SANSKRIT = "/System/Library/Fonts/Supplemental/Devanagari Sangam MN.ttc"
 FONT_PATH_TAMIL = "/Library/Fonts/NotoSansTamil-Regular.ttf"
 if os.name == 'nt': # Windows
-    FONT_PATH_SANSKRIT = "C:/Windows/Fonts/Shobhika.ttf"
-    FONT_PATH_TAMIL = "C:/Windows/Fonts/NotoSansTamil-Regular.ttf"
+    FONT_PATH_SANSKRIT = "C:/Windows/Fonts/mangal.ttf"
+    FONT_PATH_TAMIL = "C:/Windows/Fonts/Nirmala.ttf"
 FONT_SIZE_SANSKRIT = 55
 FONT_SIZE_TAMIL = 45
 LINES_PER_SLIDE = 4
 SLIDE_WIDTH = 1920
 SLIDE_HEIGHT = 1080
 LINE_SPACING = 100
+MARGIN_LEFT = 50
 MARGIN_TOP_SANSKRIT = 90
 MARGIN_TOP_TAMIL = 620
 ASTRING = "sriradhastotram"
@@ -56,35 +57,41 @@ def draw_slide(s_chunk, t_chunk, highlight_idx, slide_num, first_line_title=Fals
     draw = ImageDraw.Draw(img)
 
     first_line_fill = (0, 0, 255)
-    text_fill = (0, 0, 128)
-    text_highlight_fill = (255, 255, 255)
-    shadow_fill = (0, 0, 0)
-    shadow_highlight_fill = (173, 216, 230)
+    text_fill = (255, 255, 255)
+    text_highlight_fill = (0, 255, 255)
+    text_shadow_fill = (0, 0, 0)
+    text_shadow_highlight_fill = (0, 0, 0)
 
     no_blanks = first_line_title and len(s_chunk) == LINES_PER_SLIDE and all(line.strip() for line in s_chunk)
-    for i, line in enumerate(s_chunk):
-        # navy blue if highlighted, white otherwise
-        fill = text_fill if i == highlight_idx else text_highlight_fill
-        # shadow fill while if highlighted, light blue otherwise
-        shadow_fill = shadow_fill if i == highlight_idx else shadow_highlight_fill
-        
-        # draw text shadow
-        draw.text((52, MARGIN_TOP_SANSKRIT + i * LINE_SPACING + 2), line, font=font_sanskrit, fill=shadow_fill)
-        # is the first line and not highlighted, use blue
-        # fill = first_line_fill if i == 0 and i != highlight_idx and no_blanks else fill
-        draw.text((50, MARGIN_TOP_SANSKRIT + i * LINE_SPACING), line, font=font_sanskrit, fill=fill)
-        
-    for i, line in enumerate(t_chunk):
-        # navy blue if highlighted, white otherwise
-        fill = text_fill if i == highlight_idx else text_highlight_fill
-        # shadow fill while if highlighted, light blue otherwise
-        shadow_fill = shadow_fill if i == highlight_idx else shadow_highlight_fill
-        
-        # draw text shadow
-        draw.text((52, MARGIN_TOP_TAMIL + i * LINE_SPACING + 2), line, font=font_tamil, fill=shadow_fill)
-        # is the first line and not highlighted, use blue
-        # fill = first_line_fill if i == 0 and i != highlight_idx and no_blanks else fill
-        draw.text((50, MARGIN_TOP_TAMIL + i * LINE_SPACING), line, font=font_tamil, fill=fill)
+    def draw_lines(chunk, font, margin_top):
+        for i, line in enumerate(chunk):
+            fill = text_highlight_fill if i == highlight_idx else text_fill
+            shadow_fill = text_shadow_highlight_fill if i == highlight_idx else text_shadow_fill
+
+            # if i != highlight_idx:
+            #     # Draw shadow for better visibility
+            #     draw.text((MARGIN_LEFT+2, margin_top + i * LINE_SPACING + 2), line, font=font, fill=shadow_fill)
+            fill = first_line_fill if i == 0 and i != highlight_idx and no_blanks else fill
+            # draw a rounded corner box around the text if it's the highlighted line
+            if i == highlight_idx:
+                bbox = font.getbbox(line)
+                width = bbox[2] - bbox[0]
+                height = bbox[3] - bbox[1]
+                draw.rounded_rectangle(
+                    (
+                        MARGIN_LEFT - 10,
+                        margin_top + i * LINE_SPACING - 10,
+                        MARGIN_LEFT + width + 10,
+                        margin_top + i * LINE_SPACING + height + 10
+                    ),
+                    radius=15,
+                    fill=(0, 0, 0, 128)
+                )
+
+            draw.text((MARGIN_LEFT, margin_top + i * LINE_SPACING), line, font=font, fill=fill)
+
+    draw_lines(s_chunk, font_sanskrit, MARGIN_TOP_SANSKRIT)
+    draw_lines(t_chunk, font_tamil, MARGIN_TOP_TAMIL)
 
     img.save(HOME + f"/Downloads/{ASTRING}/{ASTRING}-Slide{slide_num:04d}.png", "PNG")
 
@@ -101,7 +108,10 @@ for i in range(0, len(sanskrit_lines), LINES_PER_SLIDE):
     #    break  # skip last chunk if incomplete
 
     for hl in range(max_lines):
-        draw_slide(s_chunk, t_chunk, hl, slide_num, first_line_title=True)
+        draw_slide(s_chunk, t_chunk, hl, slide_num, first_line_title=False)
         slide_num += 1
+
+if os.path.exists(HOME + "/Downloads/mythili-end-slide.png"):
+    shutil.copy(f"{HOME}/Downloads/mythili-end-slide.png", f"{HOME}/Downloads/{ASTRING}/{ASTRING}-Slide{slide_num:04d}.png")
 
 print(f"Done. Slides saved to {HOME}/Downloads/" + ASTRING)
